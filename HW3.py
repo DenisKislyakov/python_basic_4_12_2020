74119b6155b1167e102fe0f0a07da1dd7bb8b4d5
DenisKislyakov <hemuroff@gmail.com>
2021-11-02 15:10:15 +0300

    HW1

---
+++ HW3.py
@@ -0,0 +1,11 @@
+
+#Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
+
+alphavit = 'abcdefgjihklmnopqrstuvwxyz'
+a = input("Введите букву: ")
+b = input("Введите букву: ")
+r = alphavit.index(a)+1
+t = alphavit.index(b)+1
+print(r)
+print(t)
+print(abs(r-t)-1)
